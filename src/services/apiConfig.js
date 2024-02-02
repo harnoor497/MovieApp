a482ccbd052fc451938a9b78d6ffbeca8033a1c9
Harnoor Kaur <harnoor1009@gmail.com>
2024-02-01 20:02:34 -0800

    project added

---
+++ src/services/apiConfig.js
@@ -0,0 +1,5 @@
+export const MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/";
+export const TV_BASE_URL = "https://api.themoviedb.org/3/tv/";
+export const SEARCH_BASE_URL = "https://api.themoviedb.org/3/search/";
+export const SINGLE_BASE_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = 'f6f5be8ae525655604794e5e6224ce5b';
